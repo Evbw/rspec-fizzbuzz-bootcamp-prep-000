de3d9d4e62c86078200a9a1ab5af64847a1efec4
Everett <everett.bowline@gmail.com>
2018-03-09 02:18:23 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -8,13 +8,3 @@ def fizzbuzz(int)
   else
     "nil"
 end
-
-def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0
-    "FizzBuzz"
-  elsif int % 3 == 0
-    "Fizz"
-  elsif int % 5 == 0
-    "Buzz"
-  end
-end
